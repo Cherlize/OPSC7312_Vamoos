f3424ceb156f196fde71975bb427b375d26249fc
joyst <ernest178.em@gmail.com>
2021-11-09 12:48:57 +0200

    first commit

---
+++ app/src/main/java/com/example/mapsplatform12/MapsActivity.java
@@ -0,0 +1,216 @@
+package com.example.mapsplatform12;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.mapsplatform12.databinding.ActivityMapsBinding;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements
+        GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener,
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback, GoogleMap.OnMarkerClickListener {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    private Marker markerMenlyn;
+    private Marker markerBrooklyn;
+    private Marker markerArcadia;
+
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean permissionDenied = false;
+    private EditText edtSearch1;
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        edtSearch1 = findViewById(R.id.edtSearch);
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+      //  init();
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setOnMyLocationButtonClickListener(this);
+        map.setOnMyLocationClickListener(this);
+        enableMyLocation();
+
+        LatLng pretoria = new LatLng(-25.731340, 28.218370);
+        LatLng menlyn = new LatLng(-25.7819, 28.2768);
+        LatLng brooklyn = new LatLng(-25.7646, 28.2393);
+        LatLng arcadia = new LatLng(-25.7453, 28.2030);
+
+        // Move the camera instantly to Sydney with a zoom of 15.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pretoria, 15));
+
+// Zoom in, animating the camera.
+        map.animateCamera(CameraUpdateFactory.zoomIn());
+
+// Zoom out to zoom level 10, animating with a duration of 2 seconds.
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+// Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(menlyn)      // Sets the center of the map to Mountain View
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        // Add some markers to the map, and add a data object to each marker.
+        markerMenlyn = map.addMarker(new MarkerOptions()
+                .position(menlyn)
+                .title("Perth"));
+        markerMenlyn.setTag(0);
+
+        markerBrooklyn = map.addMarker(new MarkerOptions()
+                .position(brooklyn)
+                .title("Sydney"));
+        markerBrooklyn.setTag(0);
+
+        markerArcadia = map.addMarker(new MarkerOptions()
+                .position(arcadia)
+                .title("Brisbane"));
+        markerArcadia.setTag(0);
+
+        // Set a listener for marker click.
+        map.setOnMarkerClickListener((GoogleMap.OnMarkerClickListener) this);
+    }
+    private void geolocate(){
+
+        String searching = edtSearch1.getText().toString();
+        Geocoder geocoder = new Geocoder(MapsActivity.this);
+        List<Address>list = new ArrayList<>();
+        try {
+           list= geocoder.getFromLocationName(searching,1);
+        }
+        catch (IOException e){
+            Log.e("TAG", "geolocate: IOException: " + e.getMessage());
+        }
+        if(list.size()>0){
+            Address address = list.get(0);
+            Toast.makeText(this,address.toString(),Toast.LENGTH_SHORT).show();
+            moveCamera(new LatLng(address.getLatitude(),address.getLongitude()),17,address.getAddressLine(0));
+        }
+    }
+
+    private void init()
+    {
+        edtSearch1.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int i, KeyEvent event) {
+                if (i == EditorInfo.IME_ACTION_SEARCH ||
+                i == EditorInfo.IME_ACTION_DONE ||
+                event.getAction()==KeyEvent.ACTION_DOWN ||
+                        event.getAction()==KeyEvent.KEYCODE_ENTER)
+                {
+                    geolocate();
+                }
+                return false;
+            }
+        });
+    }
+    private void moveCamera(LatLng latLng,float zoom, String title){
+        zoom = 17;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+        MarkerOptions options= new MarkerOptions().position(latLng).title(title);
+        mMap.addMarker(options);
+    }
+
+
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (map != null) {
+                map.setMyLocationEnabled(true);
+            }
+        } else {
+            // Permission to access the location is missing. Show rationale and request permission
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        }
+    }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        return false;
+    }
+}
